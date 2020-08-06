85335d05230acdd497fd069d08e4f11a6a1aafae
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-08-06 00:23:52 -0300

    node

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,7 +4,8 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
     return (
         <div id="page-teacher-list" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas." />
+            <PageHeader title="Que incrível que você quer dar aulas."
+            description="O primeiro passo é preencher este formulário de inscrição." />
         </div>
     )
 }
