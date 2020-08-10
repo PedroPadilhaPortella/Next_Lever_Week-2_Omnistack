4fa4dafba990d754e7e7b9a5a66e45666228ab62
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-08-10 00:38:59 -0300

    mobile
    rotas, paginação e estilo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing';
 import { AppLoading } from 'expo';
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
-import { View } from 'react-native';
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -13,14 +12,13 @@ export default function App() {
     Poppins_400Regular,
     Poppins_600SemiBold,
   })
-
   if (!fontsLoaded) {
     return <AppLoading />
   } else {
     return (
       <>
-        <Landing />
-        <StatusBar style="auto" />
+        <AppStack />
+        <StatusBar style="light" />
       </>
     );
   }
