089ee6d97e2858fc8496f5e89d4ce17a950604f4
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-08-11 01:46:49 -0300

    techer list

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text} from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
 import TeacherItem from '../../components/TeacherItem';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 function TeacherList() {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matérias</Text>
+          <TextInput style={styles.input} placeholder="Qual a Matéria?" placeholderTextColor='#c1bccc'/>
+        </View>
+
+        <View style={styles.inputGroup}>
+          <View style={styles.inputBlock}>
+          <Text style={styles.label}>Dia da Semana</Text>
+          <TextInput style={styles.input} placeholder="Qual o dia?" placeholderTextColor='#c1bccc'/>
+          </View>
+
+          <View style={styles.inputBlock}>
+          <Text style={styles.label}>Horário</Text>
+          <TextInput style={styles.input} placeholder="Qual horário?" placeholderTextColor='#c1bccc'/>
+          </View>
+        </View>
+      </PageHeader>
 
       <ScrollView style={styles.teacherList}
       contentContainerStyle={{
